3246b8d102e7294c76b714edc585a6bfeb8b04ab
Ana Claudia <anacnogueira@gmail.com>
2023-04-14 15:29:59 -0300

    Working with Emiiter

---
+++ emiiter.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var handle = require('./handle');
+var events = require('events');
+var emitter = new events.EventEmitter();
+
+// emitter.on('say', say);
+
+// function say() {
+//     console.log('I am saying...')
+// }
+
+// emitter.emit('say');
+
+var server = http.createServer(handle);
+
+server.listen(3000, function() {
+    console.log('Server is listening at port 3000');
+});
